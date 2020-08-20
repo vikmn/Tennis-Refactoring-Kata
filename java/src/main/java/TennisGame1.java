ebf05a420a9bac3052d57f10fe1e8ffba99f8323
Marco Consolaro <marco@alcortech.net>
2020-08-20 08:14:03 +0100

    r removed unused fields

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -3,13 +3,6 @@ public class TennisGame1 implements TennisGame {
     
     private int m_score1 = 0;
     private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
-
-    public TennisGame1(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
-    }
 
     public void wonPoint(String playerName) {
         if (playerName == "player1")
--- java/src/test/java/TennisTest.java
+++ java/src/test/java/TennisTest.java
@@ -3,7 +3,6 @@ import static org.junit.Assert.*;
 import java.util.Arrays;
 import java.util.Collection;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -79,7 +78,7 @@ public class TennisTest {
 
     @Test
     public void checkAllScoresTennisGame1() {
-        TennisGame1 game = new TennisGame1("player1", "player2");
+        TennisGame1 game = new TennisGame1();
         checkAllScores(game);
     }
 
