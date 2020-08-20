08c9f279dce5aff180f7e0b7aaf65f8def8e2125
Marco Consolaro <marco@alcortech.net>
2020-08-20 08:26:13 +0100

    r remove else cases

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -22,33 +22,37 @@ public class TennisGame1 implements TennisGame {
                 case 2 -> "Thirty-All";
                 default -> "Deuce";
             };
-        } else if (scorePlayer1 >= 4 || scorePlayer2 >= 4) {
+            return score;
+        }
+        if (scorePlayer1 >= 4 || scorePlayer2 >= 4) {
             int minusResult = scorePlayer1 - scorePlayer2;
-            if (minusResult == 1) score = "Advantage player1";
+            if (minusResult == 1) {
+                score = "Advantage player1";
+            }
             else if (minusResult == -1) score = "Advantage player2";
             else if (minusResult >= 2) score = "Win for player1";
             else score = "Win for player2";
-        } else {
-            for (int i = 1; i < 3; i++) {
-                if (i == 1) tempScore = scorePlayer1;
-                else {
-                    score += "-";
-                    tempScore = scorePlayer2;
-                }
-                switch (tempScore) {
-                    case 0:
-                        score += "Love";
-                        break;
-                    case 1:
-                        score += "Fifteen";
-                        break;
-                    case 2:
-                        score += "Thirty";
-                        break;
-                    case 3:
-                        score += "Forty";
-                        break;
-                }
+            return score;
+        }
+        for (int i = 1; i < 3; i++) {
+            if (i == 1) tempScore = scorePlayer1;
+            else {
+                score += "-";
+                tempScore = scorePlayer2;
+            }
+            switch (tempScore) {
+                case 0:
+                    score += "Love";
+                    break;
+                case 1:
+                    score += "Fifteen";
+                    break;
+                case 2:
+                    score += "Thirty";
+                    break;
+                case 3:
+                    score += "Forty";
+                    break;
             }
         }
         return score;
