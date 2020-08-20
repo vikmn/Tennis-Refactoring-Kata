bffd48f59f3fc123b7783cc30ff064e173a2f042
Marco Consolaro <marco@alcortech.net>
2020-08-20 08:15:07 +0100

    ruse equals for string comparison

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -5,7 +5,7 @@ public class TennisGame1 implements TennisGame {
     private int m_score2 = 0;
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
+        if (playerName.equals("player1"))
             m_score1 += 1;
         else
             m_score2 += 1;
